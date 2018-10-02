12a0af2c638d9b3500273f6d29edceaa0997b20c
juniorhc <junior.jaava@gmail.com>
2018-10-02 19:35:30 +0100

    Creating a pool of connections using c3p0. Testing this pool of connections

---
+++ projeto-jpa-2/src/main/java/br/com/caelum/controller/TestaPool.java
@@ -0,0 +1,25 @@
+package br.com.caelum.controller;
+
+import java.beans.PropertyVetoException;
+import java.sql.SQLException;
+
+import br.com.caelum.JpaConfigurator;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class TestaPool {
+    public static void main(String[] args) throws PropertyVetoException, SQLException {
+        ComboPooledDataSource dataSource = (ComboPooledDataSource) new JpaConfigurator().getDataSource();
+
+        for(int i = 0; i < 10; i ++) {
+            dataSource.getConnection();
+
+            System.out.println(i + " - Conexões existentes: " + dataSource.getNumConnections());
+            System.out.println(i + " - Conexões ocupadas: " + dataSource.getNumBusyConnections());
+            System.out.println(i + " - Conexões ociosas: " + dataSource.getNumIdleConnections());
+
+            System.out.println("");
+        }
+
+    }
+}
